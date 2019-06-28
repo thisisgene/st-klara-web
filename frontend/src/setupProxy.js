501f1ac054f7062818e311f79a68ffe541c4add0
thisisgene <emdo2000+genegit@gmail.com>
2019-06-28 16:37:35 +0200

    Changed settings

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware')
+// const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-  app.use(proxy('/wp-json/*', { target: 'http://wordpress:80/' }))
-}
+// module.exports = function(app) {
+//   app.use(proxy('/wp-json/*', { target: 'http://wordpress:80/' }))
+// }
