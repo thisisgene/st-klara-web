4cb8a50bbda46185462f1eea002ae8c5ab94c874
thisisgene <emdo2000+genegit@gmail.com>
2019-07-02 18:15:13 +0200

    Changed cover image to be background image

--- frontend/src/components/layout/CoverImage/CoverImage.js
+++ frontend/src/components/layout/CoverImage/CoverImage.js
@@ -8,7 +8,8 @@ import styles from './CoverImage.module.sass'
 export default class CoverImage extends Component {
   state = {
     pages: [],
-    isLoaded: false
+    isLoaded: false,
+    makeImageFixed: false
   }
 
   componentDidMount() {
@@ -22,33 +23,46 @@ export default class CoverImage extends Component {
       )
       .catch(err => console.log(err))
   }
+
+  getImgFromPage = html => {
+    let parser = new DOMParser()
+    let htmlDoc = parser.parseFromString(html, 'text/html')
+    let newHtml = document.createElement('div')
+    newHtml.className = 'gallery-container'
+
+    let htmlCollection = htmlDoc.querySelectorAll('img')
+    let htmlArray = Array.from(htmlCollection)
+    console.log(htmlArray)
+    return htmlArray
+  }
+
   render() {
     const { isLoaded, pages } = this.state
-
+    let img = pages
+      .filter(page => page.title.rendered === 'Titelbild')
+      .map((page, index) => {
+        if (index === 0) return this.getImgFromPage(page.content.rendered)
+      })
+    console.log(img[0])
     return (
       <div
         className={cx(styles['cover-image-container'], {
           [styles['loading']]: !isLoaded
         })}
       >
-        {pages &&
-          pages
-            .filter(page => page.title.rendered === 'Titelbild')
-            .map((page, index) => (
-              <div
-                key={index}
-                className={cx(styles['cover-image'], {
-                  [styles['loading']]: !isLoaded
-                })}
-              >
-                {index === 0 && (
-                  <div
-                    className={styles['cover-image--image']}
-                    dangerouslySetInnerHTML={{ __html: page.content.rendered }}
-                  />
-                )}
-              </div>
-            ))}
+        {img &&
+          img.map((i, index) => (
+            <div>
+              {index === 0 && (
+                <div
+                  className={styles['cover-image--image']}
+                  style={{ backgroundImage: `url('${i[0].src}')` }}
+                >
+                  <img src={i[0].src} alt="" />
+                </div>
+              )}
+            </div>
+          ))}
       </div>
     )
   }
