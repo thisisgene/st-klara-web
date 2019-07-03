e9aaccaa4588f2306fd1e05f50f363c959f8831c
thisisgene <emdo2000+genegit@gmail.com>
2019-07-03 16:13:27 +0200

    Removed wp-content

--- wp-content/plugins/media-library-assistant/js/mla-define-ajaxurl-scripts.js
+++
@@ -1,5 +0,0 @@
-// defines Global ajaxurl if needed
-
-if ( ( typeof ajaxurl === 'undefined' ) && ( typeof wp.media.view.l10n.mla_strings.ajaxurl === 'string' ) ) {
-	var ajaxurl = wp.media.view.l10n.mla_strings.ajaxurl;
-}
