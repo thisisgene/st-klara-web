150aa8b361c20624a59c67316f1087ec44f7f934
thisisgene <emdo2000+genegit@gmail.com>
2019-07-04 15:22:23 +0200

    Added fs_method in wp-config

--- wp-config.php
+++ wp-config.php
@@ -92,5 +92,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
+define('FS_METHOD','direct');
+
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
